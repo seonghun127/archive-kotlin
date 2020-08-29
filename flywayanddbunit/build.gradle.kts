2cfed0997d2ed383c6731cfd2b7b3b211768dd89
seonghun127 <seonghun127@gmail.com>
2020-08-29 18:03:36 +0900

    refactor: h2 적용 및 yml 설정 추가

--- flywayanddbunit/build.gradle.kts
+++ flywayanddbunit/build.gradle.kts
@@ -23,7 +23,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    runtimeOnly("mysql:mysql-connector-java")
+//    runtimeOnly("mysql:mysql-connector-java")
+    runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
